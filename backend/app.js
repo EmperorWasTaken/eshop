f594f7ff566d61614ea561d90681c77fd492b20f
Kai Kåsa <ktkaas18@uia.no>
2022-08-07 10:57:07 +0200

    models and routes

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-require('dotenv/config');
-
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//Routes
+const categoriesRoutes = require("./routers/categories");
+const productsRoutes = require("./routers/products");
+const usersRoutes = require("./routers/users");
+const ordersRoutes = require("./routers/orders");
+
 const api = process.env.API_URL;
 
-// Routes
-const categoriesRouter = require('./routers/categories');
-const ordersRouter = require('./routers/orders');
-const productsRouter = require('./routers/products');
-const usersRouter = require('./routers/users');
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-
-app.use(express.json()); //Middleware
-app.use(morgan('tiny')); // For logging
-app.use(`${api}/categories`, categoriesRouter);
-app.use(`${api}/orders`, ordersRouter);
-app.use(`${api}/products`, productsRouter);
-app.use(`${api}/users`, usersRouter);
-
-const Product = require('./models/product')
-
-// Connect to MongoDB
-mongoose.connect(process.env.CONNECTION_STRING)
-.then(() => {
-    console.log('Database Connected!');
-})
-.catch((err) => {
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    
+  })
+  .then(() => {
+    console.log("Database Connected!");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 app.listen(3000, ()=>{
     console.log("Server is running now http://localhost:3000");
